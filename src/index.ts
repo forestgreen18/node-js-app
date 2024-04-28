6b720399a53155e3516c441ec62c1e31b84d86f9
Illia Kornyk <forest.bukson@gmail.com>
2024-04-28 20:13:49 +0300

    feat: add route

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get("/hello-world", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
